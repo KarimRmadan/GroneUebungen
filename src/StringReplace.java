6a7097eaaec0608d2c6f5bab7a2ab7295be66992
Karim <karim-rmadan@gmx.de>
2024-10-14 14:01:16 +0200

    initial commit

--- src/StringReplace.java
+++
@@ -1,9 +0,0 @@
-public class StringReplace {
-    public static void main(String[] args) {
-        String test = javax.swing.JOptionPane.showInputDialog("Testeingabe");
-
-        String ausgabe = "Test";
-
-        System.out.println(test);
-    }
-}
