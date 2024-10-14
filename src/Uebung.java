09e2a880219fb9f040352ce01061df2c882ead3c
Karim <karim-rmadan@gmx.de>
2024-10-14 13:48:42 +0200

    Uebung

---
+++ src/Uebung.java
@@ -0,0 +1,19 @@
+public class Uebung { 
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 10) {
+            System.out.println("While Durchlauf Nummer " + i);
+            i++;
+
+        }
+    int j = 10;
+do {
+    System.out.println("Durchlaufnr " + j);
+    j--;
+  
+    }
+    while (j > 0);
+    
+}
+}
+
